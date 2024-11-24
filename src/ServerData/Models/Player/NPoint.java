19736ce2ee1a3d5d51ea7f0c8b0875a7732b0b1d
Hieu <hieulc2811@gmail.com>
2024-11-24 10:54:45 +0700

    gg

--- src/ServerData/Models/Player/NPoint.java
+++ src/ServerData/Models/Player/NPoint.java
@@ -1424,9 +1424,9 @@ public class NPoint {
             }
             if (this.player.id == 11025) {
                 tiemNang *= Manager.RATE_EXP_SERVER * 10;
-            } else if (this.player.nPoint.power > 6000000000L) {
+            } else if (this.player.nPoint.power > 60_000_000_000L) {
                 tiemNang *= 1;
-            } else if (this.player.nPoint.power < 3000000000L) {
+            } else if (this.player.nPoint.power < 30_000_000_000L) {
                 tiemNang *= Manager.RATE_EXP_SERVER * 2;
             } else {
                 tiemNang *= Manager.RATE_EXP_SERVER;
